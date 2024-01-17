f710a30d3f2fb802612841ccca8115b106674ebb
vickytranr <78326917+vickytranr@users.noreply.github.com>
2024-01-16 20:53:33 -0500

    dog

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ function myFunction() {
 
 
   function changeImage() {
-    document.getElementById("dog").src = "dog.png";
-    document.getElementById("dog").src = "dog_kiss.png";
+    document.getElementById("dog").src = "dog2.png";
+    document.getElementById("dog").src = "dog2_kiss.png";
     
     setTimeout(changeImageBack,600)
     
 }
 
 function changeImageBack() {
-    document.getElementById("dog").src = "dog_kiss.png";
-    document.getElementById("dog").src = "dog.png";
+    document.getElementById("dog").src = "dog2_kiss.png";
+    document.getElementById("dog").src = "dog2.png";
     
 }
