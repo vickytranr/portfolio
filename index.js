c2ce24237e56b647413dff58ee0a8be87d6f2a0c
vickytranr <78326917+vickytranr@users.noreply.github.com>
2024-01-16 20:44:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ function myFunction() {
   }
 
 
-  
+  function changeImage() {
+    document.getElementById("dog").src = "dog.png";
+    document.getElementById("dog").src = "dog_kiss.png";
+    
+    setTimeout(changeImageBack,600)
+    
+}
+
+function changeImageBack() {
+    document.getElementById("dog").src = "dog_kiss.png";
+    document.getElementById("dog").src = "dog.png";
+    
+}
